f7f7664667f7a1b8c34f0e5400b06bb689b6c9fe
Sergii79 <tsymbal.sergiy@gmail.com>
2024-06-14 23:26:18 +0200

    commit  third

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,10 @@
 import Profile from "../Profile/Profile";
+import user from "../Profile/userData.json";
 
 export default function App() {
-  const imgURL =
-    "https://masterpiecer-images.s3.yandex.net/37d9d9d5799f11ee99fbbadf81d486ab:upscaled";
   return (
     <div>
-      <h1>Super title</h1>
-      <p>Hello React</p>
-      <img
-        src="https://masterpiecer-images.s3.yandex.net/c352b1b9801c11ee9607720ccb3e265f:upscaled"
-        alt="it is cat"
-        height={300}
-      />
-      <img src={imgURL} alt="cat 2" height={300} />
-      <Profile />
+      <Profile items={user} />
     </div>
   );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,34 @@
-export default function Profile() {
+export default function Profile({
+  items: {
+    username,
+    tag,
+    location,
+    avatar,
+    stats: { followers, views, likes },
+  },
+}) {
   return (
     <div>
       <div>
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-          alt="User avatar"
-        />
+        <img src={avatar} alt={username} />
+        <p>{username}</p>
+        <p>{tag}</p>
+        <p>{location}</p>
       </div>
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{likes}</span>
+        </li>
+      </ul>
     </div>
   );
 }
