c4c18438e7bc6e8dadf10a0a706d1d902509b59b
Sergii79 <tsymbal.sergiy@gmail.com>
2024-06-16 20:04:32 +0200

    commit third

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,9 +1,19 @@
+import css from "../FriendList/FriendListItem.module.css";
+
 const FriendListItem = ({ avatar, name, isOnline }) => {
+  const statusClasses = [css.desc];
+  if (isOnline) {
+    statusClasses.push(css.online);
+  } else {
+    statusClasses.push(css.offline);
+  }
   return (
-    <div>
+    <div className={css.section}>
       <img src={avatar} alt="Avatar" width="148" />
-      <p>{name}</p>
-      <p>{isOnline === true ? "Online" : "Offline"}</p>
+      <p className={css.subject}>{name}</p>
+      <p className={statusClasses.join(" ")}>
+        {isOnline === true ? "Online" : "Offline"}
+      </p>
     </div>
   );
 };
