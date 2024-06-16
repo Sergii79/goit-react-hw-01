4b06bd3acb773605348d70e2da79a45863e23e10
Sergii79 <tsymbal.sergiy@gmail.com>
2024-06-16 17:43:39 +0200

    commit third

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,8 @@
-import Profile from "../Profile/Profile";
-import user from "../Profile/userData.json";
 import "modern-normalize";
 import "../App/GlobalStyle.css";
+
+import Profile from "../Profile/Profile";
+import user from "../Profile/userData.json";
 
 export default function App() {
   return (
